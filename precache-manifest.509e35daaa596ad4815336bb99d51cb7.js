22c9176d79503f4d381f9bdd6f17190174d5568d
Apple MacBook Pro <admin@Apples-MacBook-Pro.local>
2018-12-31 14:13:11 +0000

    Updates

---
+++ precache-manifest.509e35daaa596ad4815336bb99d51cb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "9625c07ab09b43c73627",
+    "url": "/contactmanager/static/js/main.9625c07a.chunk.js"
+  },
+  {
+    "revision": "52efd2088fd61d6f8535",
+    "url": "/contactmanager/static/js/1.52efd208.chunk.js"
+  },
+  {
+    "revision": "9625c07ab09b43c73627",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "52efd2088fd61d6f8535",
+    "url": "/contactmanager/static/css/1.ac90f839.chunk.css"
+  },
+  {
+    "revision": "062f5edb9885072ed469a2569f86ad51",
+    "url": "/contactmanager/index.html"
+  }
+];
